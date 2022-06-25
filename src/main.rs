1971a23219ccee4153a957d648b24a3d9cd2a925
Ilya Sheprut <optozorax@gmail.com>
2022-06-25 18:57:10 +0300

    composed to work

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,163 @@
-use triangulate::Triangulate;
 use macroquad::prelude::*;
+use triangulate::Triangulate;
 
-#[derive(Default, Copy, Clone, PartialEq, PartialOrd)]
-pub struct Point {
-    x: f32,
-    y: f32,
-}
+#[derive(Debug, Clone, PartialOrd, PartialEq)]
+struct MyVec<'a>(&'a Vec2);
 
-impl Point {
-    pub fn new(x: f32, y: f32) -> Self { Point {x, y} }
-}
-
-impl std::fmt::Debug for Point {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
-    }
-}
-
-impl std::fmt::Display for Point {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
-    }
-}
-
-impl triangulate::Vertex for Point {
+impl<'a> triangulate::Vertex for MyVec<'a> {
     type Coordinate = f32;
 
     #[inline(always)]
-    fn x(&self) -> Self::Coordinate { self.x }
+    fn x(&self) -> Self::Coordinate {
+        self.0.x
+    }
 
     #[inline(always)]
-    fn y(&self) -> Self::Coordinate { self.y }
-}
-
-impl Into<Point> for (f32, f32) {
-    fn into(self) -> Point {
-        Point::new(self.0, self.1)
+    fn y(&self) -> Self::Coordinate {
+        self.0.y
     }
 }
 
-#[macroquad::main("Fractal")]
-async fn main() {
-    let poly = vec![(100.0, 100.0).into(), (300.0, 100.0).into(), (300.0, 300.0).into(), (200.0, 400.0).into(), (100.0, 300.0).into()];
+impl<'a> From<&'a Vec2> for MyVec<'a> {
+    fn from(vec: &'a Vec2) -> Self {
+        MyVec(vec)
+    }
+}
 
-    let polygons: Vec<Vec<Point>> = vec![poly];
-    let output = polygons.triangulate::<triangulate::builders::VecVecFanBuilder<_>>(&mut Vec::new()).unwrap().to_vec();
-
-    let sq2 = 1. / 2.0_f32.sqrt();
-    let mat3 = Mat3::from_cols(
-        Vec3::new(sq2, sq2, 0.).normalize(),
-        Vec3::new(-sq2, sq2, 0.).normalize(),
-        Vec3::new(100., 100., 1.),
-    ).inverse();
+fn to_mat4(mat3: Mat3) -> Mat4 {
     let cols = mat3.to_cols_array();
-    let mat = Mat4::from_cols(
+    Mat4::from_cols(
+        // Vec4::new(cols[0], cols[3], cols[6], 0.),
+        // Vec4::new(cols[1], cols[4], cols[7], 0.),
+        // Vec4::new(cols[2], cols[5], cols[8], 0.),
+        // Vec4::new(0., 0., 0., 0.),
+
         Vec4::new(cols[0], cols[1], cols[2], 0.),
         Vec4::new(cols[3], cols[4], cols[5], 0.),
         Vec4::new(cols[6], cols[7], cols[8], 0.),
         Vec4::new(0., 0., 0., 0.),
-    );
+    )
+}
+
+// Assumes that poly points defined in clockwise
+fn edge_mat(poly: &[Vec2], base_index: usize, edge_index: usize) -> Mat3 {
+    let len_base = (poly[base_index] - poly[(base_index + 1) % poly.len()]).length();
+    let b = poly[edge_index];
+    let a = poly[(edge_index + 1) % poly.len()];
+    let len_ab = (b - a).length();
+    let coef = len_ab / len_base;
+    Mat3::from_cols(
+        Vec3::new(b.x - a.x, b.y - a.y, 0.).normalize() * coef,
+        Vec3::new(-(b.y - a.y), b.x - a.x, 0.).normalize() * coef,
+        Vec3::new(a.x, a.y, 1.),
+    ).inverse()
+}
+
+fn triangulate(poly: &[Vec2]) -> Vec<(Vec2, Vec2, Vec2)> {
+    let polygons: Vec<Vec<MyVec>> = vec![poly.iter().map(MyVec).collect()];
+    polygons
+        .triangulate::<triangulate::builders::VecVecFanBuilder<_>>(&mut Vec::new())
+        .unwrap()
+        .iter()
+        .map(|x| (x[0].0.to_owned(), x[1].0.to_owned(), x[2].0.to_owned()))
+        .collect()
+}
+
+fn draw_polygon(poly: &[Vec2], mat_poly: Mat3, render_target: &mut macroquad::texture::RenderTarget, render_target_size: f32) {
+    set_camera(&Camera2D {
+        zoom: vec2(2. / render_target_size, 2. / render_target_size),
+        target: vec2(render_target_size / 2., render_target_size / 2.),
+        render_target: Some(*render_target),
+        ..Default::default()
+    });
+
+    clear_background(BLACK);
+
+    for i in triangulate(poly) {
+        draw_triangle(
+            (mat_poly.inverse() * Vec3::new(i.1.x, i.1.y, 1.)).xy(), 
+            (mat_poly.inverse() * Vec3::new(i.0.x, i.0.y, 1.)).xy(), 
+            (mat_poly.inverse() * Vec3::new(i.2.x, i.2.y, 1.)).xy(), 
+            WHITE
+        );
+    }
+
+    set_default_camera();
+}
+
+#[allow(clippy::too_many_arguments)]
+fn draw_recursive(
+    polygon_texture: Texture2D,
+    polygon_mat: Mat3,
+    prev_texture: Texture2D,
+    prev_mat: Mat3,
+    draw_target: RenderTarget,
+    draw_mat: Mat3,
+    draw_target_size: f32,
+    mat1: Mat3,
+    mat2: Mat3,
+    material: Material,
+) {
+    set_camera(&Camera2D {
+        zoom: vec2(2. / draw_target_size, 2. / draw_target_size),
+        target: vec2(draw_target_size, draw_target_size) / 2.,
+        render_target: Some(draw_target),
+        ..Default::default()
+    });
+
+    clear_background(BLACK);
+
+    material.set_uniform("_resolution", (draw_target_size, draw_target_size));
+    material.set_uniform("_texture_size", draw_target_size);
+    material.set_texture("_screen", draw_target.texture);
+
+    material.set_texture("_texture", polygon_texture);
+    material.set_uniform("_matrix", to_mat4(polygon_mat.inverse() * draw_mat));
+    gl_use_material(material);
+    draw_rectangle(0., 0., draw_target_size, draw_target_size, WHITE);
+    gl_use_default_material();
+
+    set_default_camera();
+
+    set_camera(&Camera2D {
+        zoom: vec2(2. / draw_target_size, 2. / draw_target_size),
+        target: vec2(draw_target_size, draw_target_size) / 2.,
+        render_target: Some(draw_target),
+        ..Default::default()
+    });
+    material.set_texture("_texture", prev_texture);
+
+    material.set_uniform("_matrix", to_mat4(prev_mat.inverse() * mat1 * draw_mat));
+    gl_use_material(material);
+    draw_rectangle(0., 0., draw_target_size, draw_target_size, WHITE);
+    gl_use_default_material();
+
+    material.set_uniform("_matrix", to_mat4(prev_mat.inverse() * mat2 * draw_mat));
+    gl_use_material(material);
+    draw_rectangle(0., 0., draw_target_size, draw_target_size, WHITE);
+    gl_use_default_material();
+    
+    set_default_camera();
+}
+
+#[macroquad::main("Fractal")]
+async fn main() {
+    let poly: Vec<Vec2> = vec![
+        (0.0, 0.0).into(),
+        (400.0, 0.0).into(),
+        (400.5, 200.5).into(),
+        (100.5, 300.5).into(),
+        (0.0, 200.0).into(),
+    ];
+
+    let mat_poly = (Mat3::from_translation(Vec2::new(50., 25.)) ).inverse(); // translation of original polygon
+    let mat_fractal = (Mat3::from_translation(Vec2::new(200., 100.))).inverse(); // translation of full fractal
+
+    let mat1 = edge_mat(&poly, 0, 2);
+    let mat2 = edge_mat(&poly, 0, 3);
 
     let size = 1000;
     let sizef = size as f32;
-    let render_target = {
-        let render_target = render_target(size, size);
-        render_target.texture.set_filter(FilterMode::Nearest);
-
-        set_camera(&Camera2D {
-            zoom: vec2(2. / sizef, 2. / sizef),
-            target: vec2(sizef / 2., sizef / 2.),
-            render_target: Some(render_target),
-            ..Default::default()
-        });
-
-        clear_background(LIGHTGRAY);
-
-        let count = 50;
-        for i in (0..count).map(|i| sizef/count as f32 * i as f32) {
-            draw_line(0., i, sizef, i, 2.0, BLUE);
-            draw_line(i, 0., i, sizef, 2.0, BLUE);
-        }
-
-        for i in output {
-            draw_triangle((i[0].x, i[0].y).into(), (i[1].x, i[1].y).into(), (i[2].x, i[2].y).into(), GREEN)
-        }
-
-        set_default_camera();
-
-        render_target
-    };
 
     let material = load_material(
         VERTEX_SHADER,
@@ -99,23 +168,72 @@ async fn main() {
                 ("_resolution".to_owned(), UniformType::Float2),
                 ("_matrix".to_owned(), UniformType::Mat4),
             ],
-            textures: vec!["_texture".to_owned()],
+            textures: vec!["_texture".to_owned(), "_screen".to_owned()],
             ..Default::default()
         },
     )
     .unwrap();
 
+    let now = std::time::Instant::now();
+
+    let render_target = {
+        let mut render_target = render_target(size, size);
+        render_target.texture.set_filter(FilterMode::Nearest);
+        draw_polygon(&poly, mat_poly, &mut render_target, sizef);
+        render_target
+    };
+
+    println!("ELAPSED on draw polygon: {:?}", now.elapsed());
+    let now = std::time::Instant::now();
+
+    let mut screen1 = macroquad::prelude::render_target(size, size);
+    draw_recursive(
+        render_target.texture,
+        mat_poly,
+        render_target.texture,
+        mat_poly,
+        screen1,
+        mat_fractal,
+        sizef,
+        mat1,
+        mat2,
+        material,
+    );
+
+    println!("ELAPSED on first draw: {:?}", now.elapsed());
+    let now = std::time::Instant::now();
+
+    let mut screen2 = macroquad::prelude::render_target(size, size);
+
+    for _ in 0..20 {
+        draw_recursive(
+            render_target.texture,
+            mat_poly,
+            screen1.texture,
+            mat_fractal,
+            screen2,
+            mat_fractal,
+            sizef,
+            mat1,
+            mat2,
+            material,
+        );
+        std::mem::swap(&mut screen1, &mut screen2);
+    }
+
+    println!("ELAPSED on full rendering: {:?}", now.elapsed());
+
     loop {
-        clear_background(WHITE);
-
-        material.set_uniform("_resolution", (screen_width(), screen_height()));
-        material.set_uniform("_texture_size", sizef);
-        material.set_uniform("_matrix", mat);
-        material.set_texture("_texture", render_target.texture);
-
-        gl_use_material(material);
-        draw_rectangle(0., 0., screen_width(), screen_height(), LIGHTGRAY);
-        gl_use_default_material();
+        draw_texture_ex(
+            screen1.texture,
+            0.0,
+            0.0,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(vec2(screen_width(), screen_height())),
+                ..Default::default()
+            },
+        );
 
         next_frame().await
     }
@@ -130,11 +248,17 @@ varying vec2 pixel_size;
 varying vec4 color;
 uniform sampler2D _texture;
 uniform float _texture_size;
+uniform sampler2D _screen;
 void main() {
     if (uv.x > 0. && uv.y > 0. && uv.x < 1.0 && uv.y < 1.0) {
-        gl_FragColor = texture2D(_texture, uv);
+        vec4 getted = texture2D(_texture, uv);
+        if (getted.x + getted.y + getted.z > 2.99) {
+            gl_FragColor = color;
+        } else {
+            gl_FragColor = texture2D(_screen, uv_screen);
+        }
     } else {
-        gl_FragColor = color;
+        gl_FragColor = texture2D(_screen, uv_screen);
     }
 }
 "#;
@@ -155,7 +279,7 @@ uniform vec2 _resolution;
 void main() {
     float coef = max(_resolution.x, _resolution.y);
     vec4 res = Projection * Model * vec4(position, 1);
-    // uv_screen = res2.xy / 2.0 + vec2(0.5, 0.5);
+    uv_screen = res.xy / 2.0 + vec2(0.5, 0.5);
     uv = (_matrix * vec4((texcoord * _texture_size * _resolution / coef).xy, 1.0, 0.)).xy / _texture_size;
     color = color0 / 255.0;
     gl_Position = res;
